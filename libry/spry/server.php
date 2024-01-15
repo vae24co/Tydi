33dda97c3a1179e03a2076df82f10be85bf50f14
AO™ <anthony@osawere.com>
2024-01-15 15:24:21 +0100

    Create server.php

---
+++ libry/spry/server.php
@@ -0,0 +1,73 @@
+<?php //*** ServerX » Tydi™ Framework © 2024 ∞ AO™ • @iamodao • www.osawere.com ∞ Apache License ***//
+
+class ServerX {
+
+	// • ==== referrer → ... »
+	public function referrer() {
+		if (!empty($_SERVER['HTTP_REFERER'])) {
+			return $_SERVER['HTTP_REFERER'];
+		}
+		return false;
+	}
+
+
+
+
+
+	// • ==== name → ... »
+	public static function name() {
+		if (!empty($_SERVER['SERVER_NAME'])) {
+			return $_SERVER['SERVER_NAME'];
+		}
+		return false;
+	}
+
+
+
+
+
+	// • ==== host → ... »
+	public static function host() {
+		if (!empty($_SERVER['HTTP_HOST'])) {
+			return $_SERVER['HTTP_HOST'];
+		}
+		return false;
+	}
+
+
+
+
+
+	// • ==== hostname → ... »
+	public static function hostname() {
+		if (!empty(gethostname())) {
+			return strtolower(gethostname());
+		}
+		return false;
+	}
+
+
+
+
+
+	// • ==== ip → ... »
+	public static function ip() {
+		if (!empty($_SERVER['SERVER_ADDR'])) {
+			return strtolower($_SERVER['SERVER_ADDR']);
+		}
+		return false;
+	}
+
+
+
+
+
+	// • ==== port → ... »
+	public static function port() {
+		if (!empty($_SERVER['SERVER_PORT'])) {
+			return $_SERVER['SERVER_PORT'];
+		}
+		return false;
+	}
+
+} //> end of ServerX
