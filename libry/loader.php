84faa19b687c4578c6ec1369b24cb5f667e0605e
AO™ <anthony@osawere.com>
2024-01-15 11:52:58 +0100

    Create loader.php

---
+++ libry/loader.php
@@ -0,0 +1,50 @@
+<?php //*** LoaderX » Tydi™ Framework © 2024 ∞ AO™ • @iamodao • www.osawere.com ∞ Apache License ***//
+
+class LoaderX {
+
+	// ◇ ==== properties
+	protected static $map = [];
+
+
+
+
+	// ◇ ==== register →
+	public static function register() {
+		spl_autoload_register([self::class, 'load']);
+	}
+
+
+
+
+	// ◇ ==== load →
+	public static function load($name) {
+		$path = self::file($name);
+		if ($path !== null) {
+			require $path;
+		}
+	}
+
+
+
+
+	// ◇ ==== file →
+	protected static function file($name, $path = __DIR__) {
+		$name = ltrim($name, '\\');
+		if (isset(self::$map[$name])) {
+			if (!empty($directory) && is_dir($directory)) {
+				return $directory . DIRECTORY_SEPARATOR . self::$map[$name];
+			}
+			return self::$map[$name];
+		}
+		return null;
+	}
+
+
+
+
+	// ◇ ==== init →
+	public static function init(array $map) {
+		self::$map = array_merge(self::$map, $map);
+	}
+
+} //> end of LoaderX
