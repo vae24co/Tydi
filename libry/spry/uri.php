7b74d65a25af1abaee1053feac388f0bd0f1ed0a
AO™ <anthony@osawere.com>
2024-01-15 17:51:25 +0100

    Update uri.php

--- libry/spry/uri.php
+++ libry/spry/uri.php
@@ -4,6 +4,7 @@ namespace Tydi\Spry;
 
 class UriX {
 
+	private static $init = false;
 	protected static $uri; # [is, path, query, param]
 
 
@@ -12,19 +13,22 @@ class UriX {
 
 	// • ==== init → ... »
 	public static function init() {
-		self::$uri = new \stdClass();
-		$uri = $_SERVER['REQUEST_URI'];
-		if (!empty($uri)) {
-			self::$uri->is = $uri;
-			$bits = parse_url($uri);
-			if (isset($bits['path'])) {
-				self::$uri->path = $bits['path'];
+		if (self::$init === false) {
+			self::$uri = new \stdClass();
+			$uri = $_SERVER['REQUEST_URI'];
+			if (!empty($uri)) {
+				self::$uri->is = $uri;
+				$bits = parse_url($uri);
+				if (isset($bits['path'])) {
+					self::$uri->path = $bits['path'];
+				}
+				if (isset($bits['query'])) {
+					self::$uri->query = $bits['query'];
+					self::$uri->param = [];
+					parse_str($bits['query'], self::$uri->param);
+				}
 			}
-			if (isset($bits['query'])) {
-				self::$uri->query = $bits['query'];
-				self::$uri->param = [];
-				parse_str($bits['query'], self::$uri->param);
-			}
+			self::$init = true;
 		}
 		return;
 	}
@@ -35,6 +39,7 @@ class UriX {
 
 	// • ==== is → ... »
 	public static function is() {
+		self::init();
 		if (isset(self::$uri->is)) {
 			return self::$uri->is;
 		}
