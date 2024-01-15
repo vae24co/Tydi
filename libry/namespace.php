7da65dfa038d8c4268309c6593cb479639e0bdad
AO™ <anthony@osawere.com>
2024-01-15 17:51:17 +0100

    Update namespace.php

--- libry/namespace.php
+++ libry/namespace.php
@@ -2,7 +2,11 @@
 
 $namespace = [
 	'Tydi\Spry\DebugX' => 'libry/spry/debug.php',
+	'Tydi\Spry\StringX' => 'libry/spry/string.php',
+	'Tydi\Spry\ServerX' => 'libry/spry/server.php',
 	'Tydi\Spry\UriX' => 'libry/spry/uri.php',
+	'Tydi\Spry\HttpX' => 'libry/spry/http.php',
+	'Tydi\Spry\UrlX' => 'libry/spry/url.php',
 ];
 
 return $namespace;
