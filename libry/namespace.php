57c8f77a54a8575d5eae6e38c69c1e60ecefddbb
AO™ <anthony@osawere.com>
2024-01-15 20:52:38 +0100

    Update namespace.php

--- libry/namespace.php
+++ libry/namespace.php
@@ -1,12 +1,14 @@
 <?php //*** Namespace » Tydi™ Framework © 2024 ∞ AO™ • @iamodao • www.osawere.com ∞ Apache License ***//
 
+defined('SPRY') || define('SPRY', LIBRY . 'spry' . DS);
+
 $namespace = [
-	'Tydi\Spry\DebugX' => 'libry/spry/debug.php',
-	'Tydi\Spry\StringX' => 'libry/spry/string.php',
-	'Tydi\Spry\ServerX' => 'libry/spry/server.php',
-	'Tydi\Spry\UriX' => 'libry/spry/uri.php',
-	'Tydi\Spry\HttpX' => 'libry/spry/http.php',
-	'Tydi\Spry\UrlX' => 'libry/spry/url.php',
+	'DebugX' => SPRY . 'debug.php',
+	'StringX' => SPRY . 'string.php',
+	'ServerX' => SPRY . 'server.php',
+	'UriX' => SPRY . 'uri.php',
+	'HttpX' => SPRY . 'http.php',
+	'UrlX' => SPRY . 'url.php',
 ];
 
 return $namespace;
