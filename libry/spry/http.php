0ecb1b0bcff1f6880ddec78d8dfe0674e59b1b9d
AO™ <anthony@osawere.com>
2024-01-15 20:52:43 +0100

    Update http.php

--- libry/spry/http.php
+++ libry/spry/http.php
@@ -1,14 +1,33 @@
 <?php //*** HttpX » Tydi™ Framework © 2024 ∞ AO™ • @iamodao • www.osawere.com ∞ Apache License ***//
 
-namespace Tydi\Spry;
-
 class HttpX {
 
+	// • property
 	private static $init = false;
 	protected static $port;
 	protected static $scheme;
 	protected static $domain;
 	protected static $uri;
+
+
+
+
+
+	// • ==== call → handler - undefined method » error
+	public function __call($method, $argument) {
+		$caller = __CLASS__ . '→' . $method . '()';
+		return DebugX::oversight(__CLASS__, 'Method Unreachable', $caller);
+	}
+
+
+
+
+
+	// • ==== callStatic → handler - undefined static method » error
+	public static function __callStatic($method, $argument) {
+		$caller = __CLASS__ . '::' . $method . '()';
+		return DebugX::oversight(__CLASS__, 'Static: Method Unreachable', $caller);
+	}
 
 
 
