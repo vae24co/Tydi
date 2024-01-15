080ab105ad6df78c203e3d9618f8eb2efd36f7a0
AO™ <anthony@osawere.com>
2024-01-15 20:36:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php //*** Index » Tydi™ Framework © 2024 ∞ AO™ • @iamodao • www.osawere.com ∞ Apache License ***//
 
-// ◇ Customization
+// • Customization
 const FRAMEWORK = 'Tydi';
 const ENVIRONMENT = 'DEVELOPMENT';
 const MACHINE = 'LOCAL';
 const SYSTEM = 'ONLINE';
 
 
-// ◇ Load Customizr
+// • Load Customizr File
 $file = 'customizr.php';
 if (!is_file($file)) {
 	$error = '<strong>' . FRAMEWORK . '™</strong> • Customizr Unavailable! → [<em>' . $file . '</em>]';
 	exit($error);
 }
-
-include $file;
+require $file;
+?>
