af0226ae507ec7e6b25e31c2de734505442e7682
AO™ <anthony@osawere.com>
2024-01-15 12:27:53 +0100

    Create namespace.php

---
+++ libry/namespace.php
@@ -0,0 +1,7 @@
+<?php //*** Namespace » Tydi™ Framework © 2024 ∞ AO™ • @iamodao • www.osawere.com ∞ Apache License ***//
+
+$namespace = [
+	'Tydi\Spry\DebugX' => 'libry/spry/debug.php',
+];
+
+return $namespace;
