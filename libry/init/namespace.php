a75c59c2c715215b4cb2428e08970b455405e8b5
AO™ <anthony@osawere.com>
2024-01-17 18:31:54 +0100

    Create namespace.php

---
+++ libry/init/namespace.php
@@ -0,0 +1,14 @@
+<?php //*** Namespace » Tydi™ Framework © 2024 ∞ AO™ • @iamodao • www.osawere.com ∞ Apache License ***//
+
+$namespace = [
+	'DebugX' => LIBRY['SPRY'] . 'debug.php',
+	'StringX' => LIBRY['SPRY'] . 'string.php',
+	'ServerX' => LIBRY['SPRY'] . 'server.php',
+	'UriX' => LIBRY['SPRY'] . 'uri.php',
+	'HttpX' => LIBRY['SPRY'] . 'http.php',
+	'EnvX' => LIBRY['SPRY'] . 'env.php',
+	'RouteX' => LIBRY['SPRY'] . 'route.php',
+	'PathX' => LIBRY['SPRY'] . 'path.php',
+];
+
+return $namespace;
