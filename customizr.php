cd83f4c6ac1c64ecb0935c98a1fef35f6c3d64c0
AO™ <anthony@osawere.com>
2024-01-17 18:28:48 +0100

    Update customizr.php

--- customizr.php
+++ customizr.php
@@ -10,14 +10,83 @@ defined('SYSTEM') || define('SYSTEM', 'ONLINE'); # [ONLINE|OFFLINE]
 defined('ENVIRONMENT') || define('ENVIRONMENT', 'DEVELOPMENT'); # [DEVELOPMENT|STAGING|PRODUCTION]
 defined('MACHINE') || define('MACHINE', 'LOCAL'); # [LOCAL|REMOTE]
 
-// • Directory & Path
-defined('LIBRY') || define('LIBRY', RD . 'libry' . DS);
-defined('ORIG') || define('ORIG', RD . 'orig' . DS);
-
 
 // • Clean String
 const CS = ['@<script[^>]*?>.*?</script>@si', '@<[\ /\!]*?[^<>]*?>@si', '@<style[^>]*?>.*?</style>@siU', '@
 		<![\s\S]*?--[ \t\n\r]*>@'];
+
+
+// • Directory
+$ORIG = RD . 'orig' . DS;
+$LIBRY = RD . 'libry' . DS;
+$LIBRY = [
+	'INIT' => $LIBRY . 'init' . DS,
+	'SPRY' => $LIBRY . 'spry' . DS
+];
+
+defined('LIBRY') || define('LIBRY', $LIBRY);
+
+
+// • Autoloader (LoaderX)
+$file = LIBRY['INIT'] . 'loader.php';
+if (!is_file($file)) {
+	$error = '<strong>' . FRAMEWORK . '™</strong> • LoaderX Unavailable! → [<em>' . $file . '</em>]';
+	exit($error);
+}
+require $file;
+LoaderX::init();
+$file = LIBRY['INIT'] . 'namespace.php';
+if (is_file($file)) {
+	$map = require $file;
+	LoaderX::map($map);
+}
+
+
+// • Custom Directory & Path
+$BACKEND = 'backend' . DS;
+$FRONTEND = 'frontend' . DS;
+$STORAGE = 'source' . DS . 'upload' . DS;
+$CLOUD = PS . 'cloud' . PS;
+
+$PATH = [
+	'BACKEND' => [
+		'CONTROL' => $BACKEND . 'controlizr' . DS,
+		'MODEL' => $BACKEND . 'modelizr' . DS,
+		'ORGAN' => $BACKEND . 'organizr' . DS,
+		'ROUT' => $BACKEND . 'routzr' . DS,
+		'UTIL' => $BACKEND . 'utilizr' . DS
+	],
+
+	'FRONTEND' => [
+		'LAYOUT' => $FRONTEND . 'layout' . DS,
+		'SLICE' => $FRONTEND . 'slicezr' . DS,
+		'VIEW' => $FRONTEND . 'viewzr' . DS
+	],
+
+	'STORAGE' => [
+		'AUDIO' => $STORAGE . 'audio' . DS,
+		'DOCUMENT' => $STORAGE . 'document' . DS,
+		'IMAGE' => $STORAGE . 'image' . DS,
+		'VIDEO' => $STORAGE . 'video' . DS
+	],
+
+	'ASSET' => [
+		'ASSET' => PS . 'asset' . PS,
+		'CSS' => PS . 'css' . PS,
+		'JS' => PS . 'js' . PS,
+		'MEDIA' => PS . 'media' . PS
+	],
+
+	'CLOUD' => [
+		'CLOUD' => $CLOUD,
+		'AUDIO' => $CLOUD . 'audio' . PS,
+		'DOCUMENT' => $CLOUD . 'document' . PS,
+		'IMAGE' => $CLOUD . 'image' . PS,
+		'VIDEO' => $CLOUD . 'video' . PS
+	],
+];
+
+defined('PATH') || define('PATH', $PATH);
 
 
 // • If System Offline or Undefined
@@ -26,43 +95,15 @@ if (!defined('SYSTEM') || SYSTEM === 'OFFLINE') {
 }
 
 
-// • Include Loader
-$file = LIBRY . 'loader.php';
-if (!is_file($file)) {
-	$error = '<strong>' . FRAMEWORK . '™</strong> • LoaderX Unavailable! → [<em>' . $file . '</em>]';
-	exit($error);
-}
-include $file;
-
-
-// • Autoloader (Initialize & Load files for Libry & Orig)
-LoaderX::init();
-
-$namespaces = [
-	'libry' => LIBRY,
-	'orig' => ORIG,
-];
-
-foreach ($namespaces as $namespace) {
-	$namespace .= 'namespace.php';
-	if (is_file($namespace)) {
-		$map = include $namespace;
-		LoaderX::map($map);
-	}
-}
-
-
-// • Custom Directory & Path
+// TODO: Implement Fusion to Determine ORIG Directory
 // TODO: Implement File Loader with Framework Error
-$file = LIBRY . 'path.php';
-if (!is_file($file)) {
-	$error = '<strong>' . FRAMEWORK . '™</strong> • Path Unavailable! → [<em>' . $file . '</em>]';
-	exit($error);
-}
-include $file;
 
 
-// • Load Debugger File
-if (is_file(RD . 'debug.php')) {
-	include RD . 'debug.php';
-}
+// • Load Router
+PathX::init(['ORIG' => $ORIG]);
+$file = PathX::routzr('app');
+require $file;
+
+
+// • Load Debugger
+PathX::load('DEBUG');
