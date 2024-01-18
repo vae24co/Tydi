7e9311d6d6fbe93acf8bc1f4bc156e69bfc16ffd
AO™ <anthony@osawere.com>
2024-01-18 11:17:44 +0100

    Update env.php

--- libry/spry/env.php
+++ libry/spry/env.php
@@ -3,7 +3,7 @@
 class EnvX {
 
 	// • property
-	protected static $constants;
+	protected static $constants = [];
 
 
 
@@ -35,16 +35,62 @@ class EnvX {
 			$content = file_get_contents($file);
 			$lines = array_filter(explode("\n", $content));
 			foreach ($lines as $line) {
-				list($key, $value) = explode('=', $line, 2);
-				$label = trim(strtoupper($key));
-				$label = trim($label, 'oTYDI_');
-				$input = trim($value);
-				if (!array_key_exists($label, self::$constants)) {
-					self::$constants[$label] = $input;
+				if (strlen($line) > 1 && !StringX::beginWithAny($line, ['#', '//'])) {
+					$entry = explode('=', $line);
+					list($label, $value) = $entry;
+					$label = StringX::cropBegin($label, 'oTYDI_');
+					$label = strtoupper(trim($label));
+					$value = trim($value);
+					if (StringX::beginWith($value, '"') && StringX::endWith($value, '"')) {
+						$value = StringX::crop($value, '"');
+					} elseif (StringX::beginWith($value, "'") && StringX::endWith($value, "'")) {
+						$value = StringX::crop($value, "'");
+					}
+					if (!array_key_exists($label, self::$constants)) {
+						self::$constants[$label] = $value;
+					}
 				}
 			}
 		}
 		return;
 	}
+
+
+
+
+
+	// • ==== version → get code version
+	public static function version($flag = null) {
+
+		// » get version
+		if(!empty(self::$constants['VERSION'])){
+			$version = self::$constants['VERSION'];
+			if($flag === 'NAME'){
+				$version = StringX::beforeAs($version, '-');
+			}
+			return $version;
+		}
+		return;
+
+
+
+
+
+
+
+		// » set version
+		// if (!empty($version)) {
+		// 	self::$version = $version;
+		// 	return true;
+		// }
+
+		// // » set default version
+		// if (empty(self::$version)) {
+		// 	self::$version = 'oreo';
+		// }
+
+
+	}
+
 
 } //> end of EnvX
